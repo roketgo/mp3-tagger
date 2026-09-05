8b2e0aa067b0be6235094fc0701aec2a9871d539
RoketGo <roketgo@gmail.com>
2026-09-05 15:48:18 +0900

    Add Nautilus integration source

---
+++ nautilus_mp3_tagger.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+import os, gettext, subprocess, gi
+gi.require_version("Nautilus", "4.0")
+from gi.repository import GObject, Nautilus
+LOCALDIR="/usr/share/mp3-tagger/locale"
